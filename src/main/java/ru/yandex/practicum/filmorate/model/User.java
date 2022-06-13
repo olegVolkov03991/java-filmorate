f5d83b28d78cc1de0782c4b30731e14ec01ad3af
Oleg <Pisos0399gmail.com@yandex.ru>
2022-06-13 12:16:40 +0300

    java-sprint8-hw.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -25,4 +25,15 @@ public class User {
     private String name;
     @NonNull
     private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday){
+        this.email = email;
+        this.login = login;
+        if(name == null || name.equals("")){
+            this.name = login;
+        } else {
+            this.name = name;
+        }
+        this.birthday = birthday;
+    }
 }
