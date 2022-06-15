9d46bee15f7e78356aed613859e787628c43b878
Oleg <Pisos0399gmail.com@yandex.ru>
2022-06-15 19:08:08 +0300

    java-sprint8-hw.

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class ValidationException extends RuntimeException{
     public ValidationException(String message){
         super(message);
